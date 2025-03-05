e7e1ec0521cc3af23e169e9174224ac8372910bb
juniorbliss49 <ucheeeberechukwu@gmail.com>
2025-03-05 18:57:47 +0100

    new changes

--- components/Navbar/Navbar.jsx
+++ components/Navbar/Navbar.jsx
@@ -1,26 +1,45 @@
+"use client";
 import Link from "next/link";
+import { usePathname, useRouter } from "next/navigation";
 import React from "react";
 
 function Navbar(props) {
+  const router = usePathname();
+
+  const isActive = (path) => {
+    console.log(router);
+    return router === path ? "active" : "";
+  };
+
   return (
     <ul className="list-none flex flex-row gap-5">
       <li>
-        <Link href={"/"}>Home</Link>{" "}
+        <Link href={"/"} className={isActive("/")}>
+          Home
+        </Link>{" "}
       </li>
       <li>
-        <Link href={"/about"}>About</Link>
+        <Link href={"/about"} className={isActive("/about")}>
+          About
+        </Link>
       </li>
       <li>
-        <Link href={"/services"}>Services</Link>
+        <Link href={"/services"} className={isActive("/services")}>
+          Services
+        </Link>
       </li>
       {/* <li>
         <Link href={"/"}>Vision & Mission</Link>
       </li> */}
       <li>
-        <Link href={"/supplies"}>Supplies</Link>
+        <Link href={"/supplies"} className={isActive("/supplies")}>
+          Supplies
+        </Link>
       </li>
       <li>
-        <Link href={"/contact"}>Contact Us</Link>
+        <Link href={"/contact"} className={isActive("/contact")}>
+          Contact Us
+        </Link>
       </li>
     </ul>
   );
