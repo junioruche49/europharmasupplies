c35f2132d5db9e1390328f2753cefe99631eed55
juniorbliss49 <ucheeeberechukwu@gmail.com>
2025-03-07 15:04:28 +0100

    new changes

--- app/supplies/page.js
+++ app/supplies/page.js
@@ -10,6 +10,26 @@ import image17 from "../../assets/img/image-17.avif";
 import image18 from "../../assets/img/image-18.avif";
 import image19 from "../../assets/img/image-19.avif";
 import image20 from "../../assets/img/image-20.avif";
+import image22 from "../../assets/img/image-22.webp";
+import image23 from "../../assets/img/image-23.jpg";
+import image24 from "../../assets/img/image-24.webp";
+import image25 from "../../assets/img/image-25.jpg";
+import image26 from "../../assets/img/image-26.jpg";
+import image27 from "../../assets/img/image-27.png";
+import image28 from "../../assets/img/image-28.jpg";
+import image29 from "../../assets/img/image-29.avif";
+import image30 from "../../assets/img/image-30.jpg";
+import image31 from "../../assets/img/image-31.jpg";
+import image32 from "../../assets/img/image-32.jpg";
+import image33 from "../../assets/img/image-33.jpg";
+import image34 from "../../assets/img/image-34.avif";
+import image35 from "../../assets/img/image-35.jpg";
+import image36 from "../../assets/img/image-36.jpg";
+import image from "../../assets/img/image.jpg";
+import image37 from "../../assets/img/image-37.webp";
+import image38 from "../../assets/img/image-38.jpeg";
+import image39 from "../../assets/img/image-39.jpg";
+import image40 from "../../assets/img/image-40.jpeg";
 
 function Supplies(props) {
   return (
@@ -73,7 +93,99 @@ function Supplies(props) {
           <Image src={image20} alt="image12" className=" h-72" />
           <p>Lab Supplies 10-Layer Cellstack BioFactory High Quality</p>
         </div>
-        {/* </div> */}
+        <div className="h-[22rem] w-64 flex flex-col items-center shadow-xl p-4">
+          <Image src={image22} alt="image22" className=" h-72" />
+          <p>
+            10111 Cell Culture Plates, Tissue Culture Treated, Individually
+            packaged
+          </p>
+        </div>
+        <div className="h-[22rem] w-64 flex flex-col items-center shadow-xl p-4">
+          <Image src={image23} alt="image22" className=" h-72" />
+          <p>
+            10113 Cell Culture Plates, Tissue Culture Treated, Plastic Bag
+            Package
+          </p>
+        </div>
+        <div className="h-[22rem] w-64 flex flex-col items-center shadow-xl p-4">
+          <Image src={image24} alt="image24" className=" h-72" />
+          <p>10114 Cell Culture Plates, Non-Treated, Plastic Bag Package</p>
+        </div>
+        <div className="h-[22rem] w-64 flex flex-col items-center shadow-xl p-4">
+          <Image src={image25} alt="image25" className=" h-72" />
+          <p>10115 Cell Culture Plates, Ultra-Low Particulate</p>
+        </div>
+        <div className="h-[22rem] w-64 flex flex-col items-center shadow-xl p-4">
+          <Image src={image26} alt="image25" className=" h-72" />
+          <p>10116 Cell Culture Plates, Coated</p>
+        </div>
+        <div className="h-[22rem] w-64 flex flex-col items-center shadow-xl p-4">
+          <Image src={image27} alt="image25" className=" h-72" />
+          <p>
+            10121 Cell Culture Dishes, Tissue Culture Treated, Packaged in
+            Plastic Bag
+          </p>
+        </div>
+        <div className="h-[22rem] w-64 flex flex-col items-center shadow-xl p-4">
+          <Image src={image28} alt="image25" className=" h-72" />
+          <p>10122 Cell Culture Dishes, Ultra-Low Particulate</p>
+        </div>
+        <div className="h-[22rem] w-64 flex flex-col items-center shadow-xl p-4">
+          <Image src={image29} alt="image25" className=" h-72" />
+          <p>10131 Cell Culture Flasks, Tissue Culture Treated, Standard</p>
+        </div>
+        <div className="h-[22rem] w-64 flex flex-col items-center shadow-xl p-4">
+          <Image src={image30} alt="image25" className=" h-72" />
+          <p>10132 Cell Culture Flasks, Non-Treated, Standard</p>
+        </div>
+        <div className="h-[22rem] w-64 flex flex-col items-center shadow-xl p-4">
+          <Image src={image31} alt="image25" className=" h-72" />
+          <p>10135 Cell Culture Flasks Closed System</p>
+        </div>
+        <div className="h-[22rem] w-64 flex flex-col items-center shadow-xl p-4">
+          <Image src={image32} alt="image25" className=" h-72" />
+          <p>10136 Cell Culture Flasks, Ultra-Low Particulate</p>
+        </div>
+        <div className="h-[22rem] w-64 flex flex-col items-center shadow-xl p-4">
+          <Image src={image33} alt="image25" className=" h-72" />
+          <p>
+            10141 Cell Culture Inserts, Tissue Culture Treated, PC Memberane
+          </p>
+        </div>
+        <div className="h-[22rem] w-64 flex flex-col items-center shadow-xl p-4">
+          <Image src={image34} alt="image25" className=" h-72" />
+          <p>10142 Cell Culture Inserts, Non-treated, PC Memberane</p>
+        </div>
+        <div className="h-[22rem] w-64 flex flex-col items-center shadow-xl p-4">
+          <Image src={image35} alt="image25" className=" h-72" />
+          <p>
+            10143 Cell Culture Inserts, Tissue Culture Treated, PET Memberane
+          </p>
+        </div>
+        <div className="h-[22rem] w-64 flex flex-col items-center shadow-xl p-4">
+          <Image src={image36} alt="image25" className=" h-72" />
+          <p>10144 Cell Culture Inserts, Non-treated, PET Memberane</p>
+        </div>
+        <div className="h-[22rem] w-64 flex flex-col items-center shadow-xl p-4">
+          <Image src={image} alt="image25" className=" h-72" />
+          <p>10145 Cell Culture Inserts, Insert</p>
+        </div>
+        <div className="h-[22rem] w-64 flex flex-col items-center shadow-xl p-4">
+          <Image src={image37} alt="image25" className=" h-72" />
+          <p>10191 Cell Culture Glass Bottom Products</p>
+        </div>
+        <div className="h-[22rem] w-64 flex flex-col items-center shadow-xl p-4">
+          <Image src={image38} alt="image25" className=" h-72" />
+          <p>10192 Cell Culture, Other Products</p>
+        </div>
+        <div className="h-[22rem] w-64 flex flex-col items-center shadow-xl p-4">
+          <Image src={image39} alt="image25" className=" h-72" />
+          <p>10211 Multi-Layer Cell Culture Flasks</p>
+        </div>
+        <div className="h-[22rem] w-64 flex flex-col items-center shadow-xl p-4">
+          <Image src={image40} alt="image25" className=" h-72" />
+          <p>102120101 Small Size Erlenmeyer Flasks,PC</p>
+        </div>
       </div>
     </div>
   );
